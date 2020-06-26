0677af03cf8a0a2044975a53ebe287cbfc2329a1
Nolan Darilek <nolan@thewordnerd.info>
2020-06-25 19:55:07 -0500

    Initial working version.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ FROM $BUILD_FROM
 ENV LANG C.UTF-8
 
 WORKDIR /
+RUN apk add --no-cache py3-pip py3-wheel python3-dev musl-dev gcc git && \
+    pip3 install opsdroid==0.19.0 opsdroid-homeassistant==0.1.7
 COPY start.sh /app/start.sh
 ENTRYPOINT ["/app/start.sh"]
 
--- start.sh
+++ start.sh
@@ -1,1 +1,39 @@
-echo Hello, world!
+#!/bin/sh
+
+mkdir -p /config/opsdroid/skills
+
+config_file=/config/opsdroid/opsdroid.yaml
+
+if [ ! -e "$config_file" ]; then
+cat > $config_file <<EOF
+logging:
+  level: info
+
+welcome-message: true
+
+databases:
+  sqlite:
+    path: /config/opsdroid/opsdroid.db
+
+connectors:
+  homeassistant:
+    url: http://supervisor/core/api
+    token: $SUPERVISOR_TOKEN
+
+skills:
+  example:
+    path: /config/opsdroid/skills/hello.py
+EOF
+
+cat > /config/opsdroid/skills/hello.py <<EOF
+from opsdroid.skill import Skill
+from opsdroid.matchers import match_regex
+
+class HelloSkill(Skill):
+    @match_regex(r'hi')
+    async def hello(self, message):
+        await message.respond('Hey')
+EOF
+fi
+
+opsdroid start -f /config/opsdroid/opsdroid.yaml
